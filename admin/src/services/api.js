8bbd4a4de68648b8986814c43cdcc94582bd24d6
letstrynewstuff <120053023+letstrynewstuff@users.noreply.github.com>
2025-12-22 12:29:22 -0500

    admin fix

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,58 +1,58 @@
-// import axios from "axios";
-
-
-// // Dynamic base URL (Render in production, localhost in dev)
-// const API_URL =
-//   import.meta.env.MODE === "production"
-//     ? "https://ecommerce-0ih0.onrender.com/api"
-//     : "http://localhost:5000/api";
-
-// // Axios instance
-// const api = axios.create({
-//   baseURL: API_URL,
-// });
-
-// api.interceptors.request.use((config) => {
-//   const token = localStorage.getItem("authToken");
-//   if (token) {
-//     config.headers.Authorization = `Bearer ${token}`;
-//   }
-//   return config;
-// });
-// const _res = await api.get("/admin/users", {
-//   headers: {
-//     Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
-//   },
-// });
-
-// export default api;
-
-
 import axios from "axios";
 
-// Dynamic base URL
+
+// Dynamic base URL (Render in production, localhost in dev)
 const API_URL =
   import.meta.env.MODE === "production"
     ? "https://ecommerce-0ih0.onrender.com/api"
     : "http://localhost:5000/api";
 
 // Axios instance
-const adminApi = axios.create({
+const api = axios.create({
   baseURL: API_URL,
 });
 
-// Attach ADMIN token automatically
-adminApi.interceptors.request.use(
-  (config) => {
-    const adminToken = localStorage.getItem("adminToken");
-
-    if (adminToken) {
-      config.headers.Authorization = `Bearer ${adminToken}`;
-    }
-
-    return config;
+api.interceptors.request.use((config) => {
+  const token = localStorage.getItem("authToken");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+  return config;
+});
+const _res = await api.get("/admin/users", {
+  headers: {
+    Authorization: `Bearer ${localStorage.getItem("adminToken")}`,
   },
-  (error) => Promise.reject(error)
-);
+});
 
-export default adminApi;
+export default api;
+
+
+// import axios from "axios";
+
+// // Dynamic base URL
+// const API_URL =
+//   import.meta.env.MODE === "production"
+//     ? "https://ecommerce-0ih0.onrender.com/api"
+//     : "http://localhost:5000/api";
+
+// // Axios instance
+// const adminApi = axios.create({
+//   baseURL: API_URL,
+// });
+
+// // Attach ADMIN token automatically
+// adminApi.interceptors.request.use(
+//   (config) => {
+//     const adminToken = localStorage.getItem("adminToken");
+
+//     if (adminToken) {
+//       config.headers.Authorization = `Bearer ${adminToken}`;
+//     }
+
+//     return config;
+//   },
+//   (error) => Promise.reject(error)
+// );
+
+// export default adminApi;
